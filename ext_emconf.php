9b049490a6eeed96d29026c1367543f2bcdedbbe
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2022-05-15 13:59:35 +0200

    Preperations vor typo3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,31 +1,31 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-    'title'            => 'Freifunk Helgoland Theme',
-    'description'      => 'Theme for Freifunk Helgoland',
-    'category'         => 'distribution',
-    'author'           => 'Kevin Quiatkowski',
-    'author_email'     => 'kevin@pinneberg.freifunk.net',
-    'author_company'   => 'Freifunk Pinneberg',
-    'shy'              => '',
-    'priority'         => '',
-    'module'           => '',
-    'state'            => 'beta',
-    'internal'         => '',
-    'uploadfolder'     => '0',
-    'createDirs'       => '',
-    'modify_tables'    => '',
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Freifunk Helgoland Theme',
+    'description' => 'Theme for Freifunk Helgoland',
+    'category' => 'distribution',
+    'author' => 'Kevin Quiatkowski',
+    'author_email' => 'kevin@pinneberg.freifunk.net',
+    'author_company' => 'Freifunk Pinneberg',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'lockType'         => '',
-    'version'          => '1.0.0',
-    'constraints'      => array(
-        'depends'   => array(
-            'typo3' => '8.7.0-9.5.99',
+    'lockType' => '',
+    'version' => '1.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-10.4.99',
             'ffpi_theme' => '',
-        ),
-        'conflicts' => array(),
-        'suggests'  => array(
+        ],
+        'conflicts' => [],
+        'suggests' => [
             'ffpi_nodecounter' => '',
             'ffpi_node_updates' => '',
-        ),
-    ),
-);
+        ],
+    ],
+];
